1e40b11e09035e4d456a7ba97f870b2b5b7e02e1
KagChi <yonuxbot@gmail.com>
2022-08-28 01:26:34 +0700

    chore: cleanup base codes

---
+++ readme.md
@@ -0,0 +1,12 @@
+<div align="center">
+
+<img src="https://i.kagchi.my.id/nezuko.png" alt="Logo" width="200px" height="200px" style="border-radius:50%"/>
+
+# glauncher-scripts
+
+**A Collection of usefull GrowLauncher scripts written in typescript.**
+
+[![GitHub](https://img.shields.io/github/license/KagChi/glauncher-scripts)](https://github.com/KagChi/glauncher-scripts/blob/main/LICENSE)
+[![Discord](https://discordapp.com/api/guilds/785715968608567297/embed.png)](https://nezu.my.id)
+
+</div>
--- src/df_farm.ts
+++ src/df_farm.ts
@@ -20,10 +20,7 @@ function findDirts(): void {
     for (const tile of GetTiles()) {
         if (((tile.y % 2) === 1) || (tile.x === 0) || (tile.x === 1) || (tile.x === 99) || (tile.x === 98)) {
             if ((tile.fg === 2) || (tile.bg === 14 && tile.fg === 0) || (tile.fg === 10) || (tile.fg === 4)) {
-                if (!FindPath(tile.x, tile.y - 2)) {
-                    breakBlock(tile.x, tile.y);
-                    break;
-                }
+                if (!FindPath(tile.x, tile.y - 2)) breakBlock(tile.x, tile.y); break;
             }
         }
     }
